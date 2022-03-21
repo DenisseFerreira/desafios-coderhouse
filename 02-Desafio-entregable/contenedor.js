72ba277e5f1c009b8f5510a72c2e9e4ce2188c1d
DenisseFerreira <ferreira.denisse@gmail.com>
2022-03-21 20:30:38 -0300

    Con asyn await

--- 02-Desafio-entregable/contenedor.js
+++ 02-Desafio-entregable/contenedor.js
@@ -1,56 +1,158 @@
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 class Contenedor {
 
     constructor(){
         this.misObjetos= [];
+        this.archivo = `./productos.json`;
+
+        this.existe();
+
+        const data = {
+            misObjetos: this.misObjetos,
+            archivo: this.archivo
+        }
+        fs.writeFileSync(this.archivo, JSON.stringify(data, null, '\t'));
     }
 
-    save(nuevoProducto){
+    // lee lo que tiene escrito en el archivo, en la data va el path
+    readFile(){
+        const data =  fs.readFileSync(this.archivo, 'utf-8');
+        return JSON.parse(data);
+    }
+
+    async readFileAsync (){
+        const data = await fs.promises.readFile(this.archivo, 'utf-8');
+        return JSON.parse(data);
+      }
+
+    // toma el archivo y escribe lo que esta en data
+    writeFile(data){
+        fs.writeFileSync(this.archivo, JSON.stringify(data, null, '\t'));
+    }
+
+    async writeFileAsync(data){
+        return await fs.writeFileSync(this.archivo, JSON.stringify(data, null, '\t'));
+    }
+
+    existe(){
+        const fileExists = fs.existsSync(this.archivo);
+        if(fileExists){
+            const data = this.readFile();
+            console.log(`Archivo ${this.archivo} existe, inicializo con el archivo`);
+            this.misObjetos = data.misObjetos;
+        } else {
+            console.log('Archivo no existe. creando uno nuevo');
+            this.misObjetos= [];
+            this.archivo = `./productos.json`;
+        }
+    }
+
+    async save(newObj){
+        const data = await this.readFileAsync();
+
         const p = {
-            id: this.misObjetos.length +1,
-            title: nuevoProducto.title,
-            price: nuevoProducto.price,
-            thumnail: nuevoProducto.thumnail          
+            id: uuidv4(),
+            title: newObj.title,
+            price: newObj.price,
+            thumnail: newObj.thumnail          
         }
-        this.misObjetos.push(p);
-        return p.id;
+
+        data.misObjetos.push(p);
+
+        this.writeFileAsync(data);
+        return `El id asignado es ${p.id}`;
     }
 
-    getById(id){
-        for (let i = 0; i < this.misObjetos.length; i++) {
-            const element = this.misObjetos[i];
-            if(element.id === id) return element;
-        }
-        return null;
+    // getById(id){
+    //     for (let i = 0; i < this.misObjetos.length; i++) {
+    //         const element = this.misObjetos[i];
+    //         if(element.id === id) return element;
+    //     }
+    //     return null;
+    // }
+    // getAll(){
+    //     return this.misObjetos;
+    // }
+    // deleteById(id){
+    //    return this.misObjetos.filter((item) => item.id !== id);
+    // }
+    // deleteAll(){
+    //     return this.misObjetos = [];
+    // }
+
+    async getById(id){
+    const data =  await this.readFileAsync();
+
+    for (let i = 0; i < data.misObjetos.length; i++) {
+        const element = data.misObjetos[i];
+        if(element.id === id) return element;
     }
-    getAll(){
-        return this.misObjetos;
+    return `El articulo es null`;
     }
+
+    async getAll(){
+        const data = await this.readFileAsync();
+        return data.misObjetos;
+    }
+
     // Elimina por id
-    deleteById(id){
-       return this.misObjetos.filter((item) => item.id !== id);
+    async deleteById(id){
+        const data = await this.readFileAsync();
+        let conelID = data.misObjetos.filter((item) => item.id == id );
+        data.misObjetos = data.misObjetos.filter((item) => item.id !== id );
+        if(conelID[0].id === id){
+            data.misObjetos;
+            this.writeFileAsync(data);
+            return conelID;
+        }else{
+            return null;
+        }
     }
+
     // Elimina todo
-    deleteAll(){
-        return this.misObjetos.filter((item) => item !== item);
+    async deleteAll(){
+        const data = await this.readFileAsync();
+        data.misObjetos = [];
+        this.writeFileAsync(data);
+        return data.misObjetos;
+        // return `Elimino todos ${data.misObjetos}`;
     }
 }
 const miContenedor = new Contenedor();
-
-const nuevoProducto = {
-    title: 'Escuadra',
+const newObj = {
+    title: 'Escuadra2',
     price: 1000,
     thumnail:'https://www.google.com/FiUIARDXAQ'
 }
 
-const salida = miContenedor.save(nuevoProducto);
-miContenedor.save(nuevoProducto);
-miContenedor.save(nuevoProducto);
-console.log(salida);
-const idDelObjeto = miContenedor.getById(1);
-console.log(idDelObjeto);
-const array_objetos = miContenedor.getAll();
-console.log(array_objetos);
-console.log(miContenedor.deleteById(3));
-console.log(miContenedor.deleteAll());
+miContenedor.save(newObj).then((data) =>{
+    console.log(data);
+}).catch((err) => {
+    console.log('Error al guardar', err);
+})
 
+miContenedor.getById('ee6c1715-235c-4241-b193-e50ee91256b5').then((data) =>{
+    console.log('El producto segun ese id es: ', data);
+}).catch((err) => {
+    console.log('Error al buscar el id', err);
+})
+
+miContenedor.getAll().then((data) =>{
+    console.log('Mostrando TODOS los productos: ', data);
+}).catch((err) => {
+    console.log('Error al mostrar TODOS los productos:', err);
+})
+
+miContenedor.deleteById('3').then((data) =>{
+    console.log('Eliminando segun el id: ', data);
+}).catch((err) => {
+    console.log('Error al eliminar por id', err);
+})
+
+miContenedor.deleteAll().then((data) =>{
+    console.log('Eliminando TODOS los productos ', data);
+}).catch((err) => {
+    console.log('Error al eliminar todos los productos', err);
+})
 

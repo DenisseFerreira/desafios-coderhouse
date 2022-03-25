71bd153f0085f7f1af533b6f769ad20329af5d9c
DenisseFerreira <ferreira.denisse@gmail.com>
2022-03-25 07:42:52 -0300

    Rutas para todos y random

---
+++ 02-Desafio-entregable/README.md
@@ -0,0 +1,10 @@
+# Contenedor de productos
+
+## Datos útiles  
+
+
+Corre `$ npm install`  
+
+Luego `$ node contenedor.js`  
+
+Ahora puedes probar agregando, eliminado o buscando productos.
---
+++ 02-Desafio-entregable/main.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const {Contenedor} = require ('./contenedor');
+
+const app = express();
+const port = 8080;
+const server = app.listen(port, () =>
+  console.log('Servidor escuchando en puerto', port)
+);
+
+server.on('error', (err) => {
+  console.log('ERROR ATAJADO', err);
+});
+
+app.get('/productos', async (req, res) => {
+  const resultado = await Contenedor.getAll();
+  res.json({
+    resultado,
+  });
+});
+
+app.get('/productoRandom', async (req, res) => {
+  const resultProductos = await Contenedor.getAll();
+  const between = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min);
+  };
+  const salida = resultProductos.map((a) => a.id);
+  console.log(salida);
+  const random = between(0, salida.length);
+  console.log('RANDOM', random);
+  const idRandom = salida[random];
+  console.log('IDRANDOM', idRandom);
+  const resultado = await Contenedor.getById(idRandom);
+  res.json({
+    resultado,
+  });
+});
+

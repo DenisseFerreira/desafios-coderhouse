59183d7a997806b744f36337b8aa62d8736631d2
DenisseFerreira <ferreira.denisse@gmail.com>
2022-09-10 20:49:48 -0400

    fix: eliminando passport

--- Proyecto-Final-04/src/services/server.js
+++ Proyecto-Final-04/src/services/server.js
@@ -1,17 +1,12 @@
 import express from 'express';
 import mainRouter from '../routes/index';
 import http from 'http';
-
 import session from 'express-session';
-import passport from 'passport';
-import { registroFunc, loginFunc } from './auth';
 
 const app = express();
 
 app.use(express.json());
-
 app.use('/api', mainRouter);
-
 app.use(function (err, req, res, next) {
   return res.status('500').json({
     msg: 'There was an unexpected error',
@@ -29,17 +24,5 @@ app.use(
     saveUninitialized: true,
   }),
 );
-
-//Indicamos que vamos a usar passport en todas nuestras rutas
-app.use(passport.initialize());
-
-//Permitimos que passport pueda manipular las sessiones de nuestra app
-app.use(passport.session());
-
-// Cuando un usuario se autentique correctamente, passport va a devolver en la session la info del usuario
-passport.use('login', loginFunc);
-
-//signUpFunc va a ser una funcion que vamos a crear y va a tener la logica de REGISTRO de nuevos usuarios
-passport.use('registro', registroFunc);
 
 export default httpServer;

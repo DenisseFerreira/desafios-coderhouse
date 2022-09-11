1459e263463b47848f9bfa2a0cd7de02ba476a78
DenisseFerreira <ferreira.denisse@gmail.com>
2022-09-10 23:03:49 -0400

    fix: ruta del login

--- Proyecto-Final-04/src/routes/loginRouter.js
+++ Proyecto-Final-04/src/routes/loginRouter.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
 import { getlogin, login, updateLogin } from '../controller/loginController';
 const router = Router();
-import passport from 'passport';
-const passportOptions = { };
-
 
 router.get('/', getlogin);
-// router.post('/',   passport.authenticate('login', passportOptions),
-// (req, res) => {
-//   res.json({ msg: 'Welcome!', user: req.user });
-// });
 router.post('/', login);
 router.put('/:email', updateLogin);
 

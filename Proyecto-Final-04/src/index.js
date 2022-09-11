98e4b7192aded74c650deb9827de130128733c59
DenisseFerreira <ferreira.denisse@gmail.com>
2022-09-10 20:48:24 -0400

    feat: implementando token

--- Proyecto-Final-04/src/index.js
+++ Proyecto-Final-04/src/index.js
@@ -4,7 +4,7 @@ import { initMongoDB } from './services/database';
 
 const init = async () => {
   await initMongoDB();
-  const puerto = process.env.PORT || 8085;
+  const puerto = process.env.PORT || 8080;
 
   server.listen(puerto, () => console.log(`SERVER UP ON PORT ${puerto}`));
 };
--- Proyecto-Final-04/src/models/user.js
+++ Proyecto-Final-04/src/models/user.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 export const userCollectionName = 'user';
 
-const userSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
     nombre: { type: String, required: false },
     apellido: { type: String, required: false },
@@ -17,10 +18,22 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true, versionKey: false }
 );
+    UserSchema.pre('save', async function (next) {
+      const user = this;
+      // console.log('this del user',user)
+      const hash = await bcrypt.hash(user.password, 10);
+      // console.log('el hash',hash);
+      this.password = hash;
+      next();
+    });
+
+    UserSchema.methods.isValidPassword = async function (password) {
+      const user = this;
+      // console.log('user isValidPassword',user);
+      const compare = await bcrypt.compare(password, user.password);
+      return compare;
+    };
  
-export const UserModel = mongoose.model(
-  userCollectionName,
-  userSchema
-);
+export const UserModel = mongoose.model( userCollectionName,  UserSchema );
 
 
